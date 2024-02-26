6e47b277671575ff8cc97c0328f8ae9a4d40ee48
albagomez1995 <152968501+albagomez1995@users.noreply.github.com>
2024-02-26 17:35:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+/*let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+
+
+if (num1 > num2 && num3){
+
+    console.log(num1);
+
+}else if (num2 > num1 && num3){
+
+    console.log(num2);
+
+}else{
+
+    console.log(num3);
+
+}*/
+
+
+//let calculadora= document.getElementById("calculadora");
+
+//calculadora.style.background = "red";
+
+
+
+let button = document.getElementById("calcular");
+
+button.addEventListener("click", calcular);
+
+function calcular() {
+
+    let peso = document.getElementById("peso")
+
+//console.log("click");
+
+}
